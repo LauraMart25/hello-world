402af8e8322d17c2b105342ecf346a3e073bf1f1
LauraMart25 <150722841+LauraMart25@users.noreply.github.com>
2025-02-16 19:01:16 -0800

    Add files via upload

---
+++ my_functions.cpp
@@ -0,0 +1,22 @@
+/**
+ * Complete this source code file
+ * Yes, this is supposed to be an easy assignment.
+ */
+
+/// YOUR WELCOME
+#include "my_functions.hpp"
+
+
+//
+#include <iostream>
+
+
+//
+int hello_world()
+{
+	//	TODO: Your code here
+	
+	return 0;
+}
+
+
